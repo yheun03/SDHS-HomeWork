ab0b4e156625ad270fd71d5a1e0920d45f9576b2
Nyoung_Ni <71180175+yheun03@users.noreply.github.com>
2020-09-30 00:47:49 +0900

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-    
+    // NOTE: intro
+    $("body > a").on("mouseover", function(){
+        $(this).css({"width":"36%"}).siblings().css({"width":"32%"})
+        $("body > a").on("mouseleave", function(){
+            $("body > a").css({"width":"calc(100%/3)"})
+        })
+    })
 })
